0d294dd9ebb384527e1bdd721595074af7cbba6b
Bill <clintoncsb@gmail.com>
2023-11-13 15:11:35 -0300

    🐛 fix: correct routers code

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const auth = require('./auth');
 const joiSchemas = require('./joiSchemas');
 const validateLogin = require('./validateLogin');
+const validateUser = require('./validateUser');
 
 module.exports = {
   auth,
   joiSchemas,
   validateLogin,
+  validateUser,
 };
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,6 +3,6 @@ const userRouter = require('express').Router();
 const userController = require('../controllers/user.controller');
 const { validateUser } = require('../middlewares');
 
-userRouter.post('/', validateUser, userController.login);
+userRouter.post('/', validateUser.validateUser, userController.create);
 
 module.exports = userRouter;
